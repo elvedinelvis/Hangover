455a1f213fd92d84950290db40ff7b02ca5b2b01
elvedinelvis <elvis@Elvedins-Air.lan>
2015-02-18 18:16:02 +0100

    JTextArea with JScrollPane
    Made a textarea which acts as a console. It changes the default console
    to this GUI JTextArea and prints every System.out.print to this area.
    Also added scrollbars.

---
+++ TextWindow.java
@@ -0,0 +1,63 @@
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+
+/**
+ * Write a description of class TextWindow here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TextWindow extends JTextArea
+{   
+    /**
+     * Constructor for objects of class TextWindow
+     */
+    public TextWindow()
+    {
+        // initialise instance variables
+        makeWindow();
+    }
+    
+    private void makeWindow()
+    {
+        setEditable(false);
+        setVisible(true);
+        
+        //Needed to make every text printed to the console, printed to this textarea instead.
+        redirectSystemStreams();
+    }
+    
+    private void updateTextArea(final String text)
+    { 
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run() {
+                append(text);
+            }
+        });
+    }
+ 
+    private void redirectSystemStreams() {
+        OutputStream out = new OutputStream(){
+            @Override
+            public void write(int b) throws IOException{
+              updateTextArea(String.valueOf((char) b));
+            }
+         
+            @Override
+            public void write(byte[] b, int off, int len) throws IOException{
+              updateTextArea(new String(b, off, len));
+            }
+         
+            @Override
+            public void write(byte[] b) throws IOException{
+              write(b, 0, b.length);
+            }
+        };
+        System.setOut(new PrintStream(out, true));
+        System.setErr(new PrintStream(out, true));
+    }
+}
